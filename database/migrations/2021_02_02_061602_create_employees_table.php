9835fb0f47c449e4dd48b57238a13a383b33c798
Brian Valencia <qbmsvalencia@tip.edu.ph>
2021-02-23 04:32:12 +0800

    Added remember token to employee migration

--- database/migrations/2021_02_02_061602_create_employees_table.php
+++ database/migrations/2021_02_02_061602_create_employees_table.php
@@ -24,6 +24,7 @@ class CreateEmployeesTable extends Migration
             $table->string('email');
             $table->string('password');
             $table->boolean('active_status');
+            $table->rememberToken();
             $table->timestamps();
         });
     }
